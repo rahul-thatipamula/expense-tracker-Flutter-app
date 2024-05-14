33bcb260e4205e62bc78509df7feef929378c3b5
rahul-thatipamula <rahulthatipamula6@gmail.com>
2024-05-14 21:41:59 +0530

    App default theming is updated (Material theme).

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
+  var kColorScheme =
+      ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+  var kDarkColorScheme = ColorScheme.fromSeed(
+      brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Expenses(),
-    );
+        darkTheme: ThemeData.dark().copyWith(
+            colorScheme: kDarkColorScheme,
+            cardTheme: CardTheme().copyWith(
+                color: kDarkColorScheme.secondaryContainer,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 8)),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: kDarkColorScheme.primaryContainer))),
+        home: Expenses(),
+        theme: ThemeData().copyWith(
+            appBarTheme: AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.onPrimary,
+            ),
+            cardTheme: CardTheme().copyWith(
+                color: kColorScheme.secondaryContainer,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 8)),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: kColorScheme.primaryContainer)),
+            textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 14)),
+            colorScheme: kColorScheme));
   }
 }
