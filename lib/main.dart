8dc8ac21d87a31e77d5677b0e104e2cdbeec3742
rahul-thatipamula <rahulthatipamula6@gmail.com>
2024-05-15 10:29:24 +0530

    device Orientation set to only portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) {
+    runApp(MainApp());
+  });
 }
 
 class MainApp extends StatelessWidget {
-  MainApp({super.key});
-
   var kColorScheme =
       ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
   var kDarkColorScheme = ColorScheme.fromSeed(
       brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
